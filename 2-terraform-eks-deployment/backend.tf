ff8637c6380de98090c0c874c95e4a8b8057abf4
gdlimbani <gautamlimbani@gmail.com>
2025-02-12 15:29:59 +0530

    Change in the bucket and the region

--- 2-terraform-eks-deployment/backend.tf
+++ 2-terraform-eks-deployment/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "jenkins-app-kub-2024-v2"
-    region = "eu-west-3"
+    bucket = "gdl-jenkins-app-kub-2025-v2"
+    region = "ap-south-1"
     key = "eks/terraform.tfstate"
   }
 }
