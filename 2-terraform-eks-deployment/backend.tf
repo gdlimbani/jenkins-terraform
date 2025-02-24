b01be1f69878159339c6060c55bbe3b1e83b433b
Gautam Limbani <gautamlimbani@yahoo.com>
2025-02-24 12:17:01 +0530

    Changes to make this repo for creating/destroying the Jenkins sever at AWS EC2 instance

--- 2-terraform-eks-deployment/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "gdl-jenkins-app-kub-2025-v2"
-    region = "ap-south-1"
-    key = "eks/gdl-terraform.tfstate"
-  }
-}
--- 2-terraform-eks-deployment/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "vpc_cidr_block" {}
-variable "private_subnet_cidr_blocks" {}
-variable "public_subnet_cidr_blocks" {}
-variable "env_prefix" {}
